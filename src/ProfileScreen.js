dccac7912026c4353eecb2559227889721925cc2
Aaron G <aaronhgross@gmail.com>
2018-04-18 11:22:09 -0400

    adding app files

---
+++ src/ProfileScreen.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import SupList from './SupList';
+
+const sups = [
+    {id:'1', author: 'Jonathan', body:'Love to climb mountains and take pictures!', time: new Date(), image: "../images/jonathan.jpg"},
+    {id:'2', author: 'Aaron', body:'Do you think I have imposter syndrome?', time: new Date(), image: "../images/aaron.jpg"},
+    {id:'3', author: 'Illia', body:'In Ukraine, tv watch you.', time: new Date(), image: "../images/illia.jpg"},
+    {id:'4', author: 'Illia', body:'What’s the definition of a Russian string quartet? A Soviet orchestra back from a US tour.', time: new Date(), image: "../images/illia.jpg"}
+]
+
+let ProfileScreen = ({match}) => { // destructures match from React props 
+    let userName = match.params.id;
+    let newSups = filterSups(sups, userName);
+    return( 
+    <SupList sups={newSups} />
+    )
+}
+
+let filterSups = (sups, userName) => {
+    return sups.filter(sup => userName === sup.author);
+}
+
+export default ProfileScreen
